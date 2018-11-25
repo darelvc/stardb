63cd30c364b82fcc0538aa99b46e380735cf9e79
Oleg Bloshenkin <darelvc@gmail.com>
2018-11-25 21:14:50 +0200

    Add error-button component and people-page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,15 +4,16 @@ import './app.css';
 
 import Header from '../header/header';
 import RandomPlanet from "../random-planet/random-planet";
-import ItemList from "../item-list/item-list";
-import PersonDetails from "../person-details/person-details";
+import ErrorIndicator from "../error-indicator/error-indicator";
+import ErrorButton from "../error-button/error-button";
+import PeoplePage from "../people-page/people-page";
 
 
 export default class App extends Component {
 
     state = {
         showRandomPlanet: true,
-        selectedPerson: 5
+        hasError: false
     };
 
     toggleRandomPlanet = () => {
@@ -23,13 +24,17 @@ export default class App extends Component {
         });
     };
 
-    onPersonSelected = (id) => {
-      this.setState({
-          selectedPerson: id
-      })
-    };
+    componentDidCatch() {
+        this.setState({
+            hasError: true
+        })
+    }
 
     render() {
+
+        if (this.state.hasError) {
+            return <ErrorIndicator />
+        }
 
         const planet = this.state.showRandomPlanet ?
             <RandomPlanet/> :
@@ -40,20 +45,17 @@ export default class App extends Component {
                 <Header />
                 { planet }
 
-                <button
-                    className="toggle-planet btn btn-warning btn-lg"
-                    onClick={this.toggleRandomPlanet}>
-                    Toggle Random Planet
-                </button>
-
-                <div className="row mb2">
-                    <div className="col-md-6">
-                        <ItemList onItemSelected={this.onPersonSelected}/>
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={this.state.selectedPerson} />
-                    </div>
+                <div className="row mb2 button-row">
+                    <button
+                        className="toggle-planet btn btn-warning btn-lg"
+                        onClick={this.toggleRandomPlanet}>
+                        Toggle Random Planet
+                    </button>
+                    <ErrorButton />
                 </div>
+
+                <PeoplePage />
+
             </div>
         );
     }
